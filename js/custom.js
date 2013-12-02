06f6c461e3a6231a0beb78a594d4e3b25ca904ab
Drew Bailey <drewmbailey@gmail.com>
2013-12-01 22:14:54 -0500

    Updates to custom.js. Comments still not changing on click.

--- js/custom.js
+++ js/custom.js
@@ -1,59 +1,60 @@
-// ACCORDION 
-  $(function() {
-    $( "#accordion" ).accordion({autoHeight: "content" });
-  });
+jQuery(function($){
 
-//  CHANGE VIDEO BY CLICKING THUMBNAIL LINK
-  $(document).ready(function(){
-   
-    $('.vid_button').click( function(e){
-      e.preventDefault();
-      var URL = $(this).attr('href');
-      var htm = '<iframe id="player" frameborder="0" allowfullscreen="1" title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/' + URL + '?enablejsapi=1"></iframe>';
-     
-    $('#player').html(htm);
-      return false;
-    });
-  });
+// ACCORDION 
+  $( "#accordion" ).accordion({autoHeight: "content" });
 
 //  LOAD MOST RECENT VIDEO ON PAGE LOAD
-  $(document).ready(function(){
-    $("#mostRecent").click()
-  });
+  $("#mostRecent").click();
+  
+//  CHANGE VIDEO BY CLICKING THUMBNAIL LINK
+  $('.vid_button').click(function(e){
+    e.preventDefault();
 
-// YOUTUBE COMMENTS
-  function getYouTubeInfo() {
-    $.ajax({
-      url: "http://gdata.youtube.com/feeds/api/videos/DMPKLJhT8uQ?v=2&alt=json",
-      dataType: "jsonp",
-      success: function (data) { parseresults(data); }
-    });
-  }
+    var URL = $(this).attr('href');
+    var htm = '<iframe id="player" frameborder="0" allowfullscreen="1" title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/' + URL + '?enablejsapi=1"></iframe>';
+    var SetURL = "http://gdata.youtube.com/feeds/api/videos/" + URL + "?v=2&alt=json";
 
-  function parseresults(data) {
-    var title = data.entry.title.$t;
-    var description = data.entry.media$group.media$description.$t;
-    var viewcount = data.entry.yt$statistics.viewCount;
-    var author = data.entry.author[0].name.$t;
-    $('#title').html(title);
-    $('#description').html('<b>Description</b>: ' + description);
-    //$('#extrainfo').html('<b>Author</b>: ' + author + '<br/><b>Views</b>: ' + viewcount);
-    getComments(data.entry.gd$comments.gd$feedLink.href + '&max-results=50&alt=json', 1);
-  }
+    console.log(SetURL);
 
-  function getComments(commentsURL, startIndex) {
-    $.ajax({
-      url: commentsURL + '&start-index=' + startIndex,
-      dataType: "jsonp",
-      success: function (data) {
-      $.each(data.feed.entry, function(key, val) {
-        $('#comments').append('<h4>' + val.author[0].name.$t + '</h4>' + '<p>' + val.content.$t + '</p>');
+    $('#player').html(htm);
+      return false;
+
+  // YOUTUBE COMMENTS
+    function getYouTubeInfo() {
+      $.ajax({
+        //url: "http://gdata.youtube.com/feeds/api/videos/" + SetURL + "?v=2&alt=json",
+        url: SetURL,
+        dataType: "jsonp",
+        success: function (data) { parseresults(data); }
       });
-      if ($(data.feed.entry).size() == 50) { getComments(commentsURL, startIndex + 50); }
-      }
-    });
-  }
+    }
 
-  $(document).ready(function () {
+    function parseresults(data) {
+      var title = data.entry.title.$t;
+      var description = data.entry.media$group.media$description.$t;
+      var viewcount = data.entry.yt$statistics.viewCount;
+      var author = data.entry.author[0].name.$t;
+      $('#title').html(title);
+      $('#description').html('<b>Description</b>: ' + description);
+      //$('#extrainfo').html('<b>Author</b>: ' + author + '<br/><b>Views</b>: ' + viewcount);
+      getComments(data.entry.gd$comments.gd$feedLink.href + '&max-results=50&alt=json', 1);
+    }
+
+    function getComments(commentsURL, startIndex) {
+      $.ajax({
+        url: commentsURL + '&start-index=' + startIndex,
+        dataType: "jsonp",
+        success: function (data) {
+        $.each(data.feed.entry, function(key, val) {
+          $('#comments').append('<h4>' + val.author[0].name.$t + '</h4>' + '<p>' + val.content.$t + '</p>');
+        });
+        if ($(data.feed.entry).size() == 50) { getComments(commentsURL, startIndex + 50); }
+        }
+      });
+    }
+
     getYouTubeInfo();
+
   });
+
+});    
