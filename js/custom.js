45e24da406703a7ca3ffe92d552cb34398bd074a
drewmbailey <drewmbailey@gmail.com>
2013-12-09 17:04:23 -0500

    Set up sharing button/links.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 jQuery(function($){
   console.log('document ready');
   // ACCORDION 
-  $( '#accordion' ).accordion({autoHeight: 'content' });
+  $( "#accordion" ).accordion({heightstyle: "content" });
   
   // CHANGE VIDEO BY CLICKING THUMBNAIL LINK
   $('.vid_button').on('click', function(){
@@ -34,7 +34,6 @@ jQuery(function($){
 
     $.ajax({
       url: 'http://gdata.youtube.com/feeds/api/videos/' + youtubeID + '?v=2&alt=json',
-      // url: SetURL,
       dataType: 'jsonp',
       success: function (data) {
         console.log('success');
@@ -45,6 +44,33 @@ jQuery(function($){
         console.log('error');
       }
     });
+
+    //SHARE LINK
+    $("#shareButton").on("click", function(){
+      console.log("button clicked");
+      //$("#sharePopup").css("display", "inline");
+      $("#sharePopup").show();
+    });
+
+    $(document).on("click", function(e) {
+      if (e.target.id != "shareButton" && !$("#shareButton").find(e.target).length) {
+        $("#sharePopup").hide();
+      }
+    });
+
+    var shareList =
+      '<ul>' 
+        + '<li><a href="http://twitter.com/home?status=http://youtu.be/' + youtubeID + '" target="_blank"><div class="shareItems" id="twitterButton"></div></a></li>'
+        + '<li><a href=""  target="_blank"><div class="shareItems" id="vimeoButton"></div></a></li>'
+        + '<li><a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://youtu.be/' + youtubeID + '&p[images][0]=&p[title]=&p[summary]="  target="_blank"><div class="shareItems" id="facebookButton"></div></a></li>'
+        + '<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http://youtu.be/' + youtubeID + '&title=&summary=&source="  target="_blank"><div class="shareItems" id="linkedinButton"></div></a></li>'
+        + '<li><a href="https://www.tumblr.com/login?share_redirect_to=%2Fshare%2Flink%3Furl%3Dhttp%253A%252F%252Fyoutu.be%252F' + youtubeID + '"  target="_blank"><div class="shareItems" id="tumblrButton"></div></a></li>'
+        + '<li><a href="https://plus.google.com/share?url=http://youtu.be/' + youtubeID + '"  target="_blank"><div class="shareItems" id="gplusButton"></div></a></li>'
+      + '</ul>';
+
+    $("#sharePopup").html(shareList); 
+    console.log(shareList);
+
   }
 
   // PARSE COMMENTS
@@ -53,7 +79,7 @@ jQuery(function($){
     var description = data.entry.media$group.media$description.$t;
     var viewcount = data.entry.yt$statistics.viewCount;
     var author = data.entry.author[0].name.$t;
-    $('#title').html(title);
+    $('#title h2').html(title);
     $('#description').html('<b>Description</b>: ' + description);
     //$('#extrainfo').html('<b>Author</b>: ' + author + '<br/><b>Views</b>: ' + viewcount);
     getComments(data.entry.gd$comments.gd$feedLink.href + '&max-results=50&alt=json', 1);
@@ -72,5 +98,31 @@ jQuery(function($){
       }
     });
   }
-  
+
+  //SHARE LINK
+  $("#shareButton").on("click", function(){
+    console.log("button clicked");
+    //$("#sharePopup").css("display", "inline");
+    $("#sharePopup").show();
+  });
+
+  $(document).on("click", function(e) {
+    if (e.target.id != "shareButton" && !$("#shareButton").find(e.target).length) {
+      $("#sharePopup").hide();
+    }
+  });
+
+  var shareList =
+    '<ul>' 
+      + '<li><a href="http://twitter.com/home?status=http://youtu.be/' + youtubeID + '"><div class="shareItems" id="twitterButton"></div></a></li>'
+      + '<li><a href=""><div class="shareItems" id="vimeoButton"></div></a></li>'
+      + '<li><a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://youtu.be/-oTndxsUdnk&p[images][0]=&p[title]=&p[summary]="><div class="shareItems" id="facebookButton"></div></a></li>'
+      + '<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http://youtu.be/DMPKLJhT8uQ&title=&summary=&source="><div class="shareItems" id="linkedinButton"></div></a></li>'
+      + '<li><a href="https://www.tumblr.com/login?share_redirect_to=%2Fshare%2Flink%3Furl%3Dhttp%253A%252F%252Fyoutu.be%252FDMPKLJhT8uQ"><div class="shareItems" id="tumblrButton"></div></a></li>'
+      + '<li><a href="https://plus.google.com/share?url=http://youtu.be/-oTndxsUdnk"><div class="shareItems" id="gplusButton"></div></a></li>'
+    + '</ul>';
+
+  $("#sharePopup").html(shareList); 
+  console.log(shareList)
+
 });
