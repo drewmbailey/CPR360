3350ccd3d92641a088742f0ef14912cb2d7e13dc
Drew Bailey <drewmbailey@gmail.com>
2013-12-08 22:34:23 -0500

    Started creating separate functions for each video.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 jQuery(function($){
 
 // ACCORDION 
-  $( "#accordion" ).accordion({autoHeight: "content" });
+  $( "#accordion" ).accordion({heighstyle: "fill" });
 
 //  LOAD MOST RECENT VIDEO ON PAGE LOAD
   $(document).ready(function() {
@@ -14,20 +14,21 @@ jQuery(function($){
 
     var URL = $(this).attr('href');
     var htm = '<iframe id="player" frameborder="0" allowfullscreen="1" title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/' + URL + '?enablejsapi=1"></iframe>';
-    var SetURL = "http://gdata.youtube.com/feeds/api/videos/" + URL + "?v=2&alt=json";
-
-    console.log(SetURL);
+    //var SetURL = "http://gdata.youtube.com/feeds/api/videos/" + URL + "?v=2&alt=json";
 
     $('#player').html(htm);
       return false;
-
   });
-  
-// YOUTUBE COMMENTS
+
+// YOUTUBE DESCRIPTION AND COMMENTS
+  //vid1
+  $('.vid1').click(function(e){
+    e.preventDefault();
+    
     function getYouTubeInfo() {
       $.ajax({
         url: "http://gdata.youtube.com/feeds/api/videos/DMPKLJhT8uQ?v=2&alt=json",
-        // url: SetURL,
+        //url: SetURL,
         dataType: "jsonp",
         success: function (data) { parseresults(data); }
       });
@@ -58,4 +59,49 @@ jQuery(function($){
     }
 
     getYouTubeInfo();
+
+  });
+
+
+  //vid2
+  $('.vid2').click(function(e){
+    e.preventDefault();
+    
+    function getYouTubeInfo() {
+      $.ajax({
+        url: "http://gdata.youtube.com/feeds/api/videos/-oTndxsUdnk?v=2&alt=json",
+        //url: SetURL,
+        dataType: "jsonp",
+        success: function (data) { parseresults(data); }
+      });
+    }
+
+    function parseresults(data) {
+      var title = data.entry.title.$t;
+      var description = data.entry.media$group.media$description.$t;
+      var viewcount = data.entry.yt$statistics.viewCount;
+      var author = data.entry.author[0].name.$t;
+      $('#title').html(title);
+      $('#description').html('<b>Description</b>: ' + description);
+      //$('#extrainfo').html('<b>Author</b>: ' + author + '<br/><b>Views</b>: ' + viewcount);
+      getComments(data.entry.gd$comments.gd$feedLink.href + '&max-results=50&alt=json', 1);
+    }
+
+    function getComments(commentsURL, startIndex) {
+      $.ajax({
+        url: commentsURL + '&start-index=' + startIndex,
+        dataType: "jsonp",
+        success: function (data) {
+        $.each(data.feed.entry, function(key, val) {
+          $('#comments').append('<h4>' + val.author[0].name.$t + '</h4>' + '<p>' + val.content.$t + '</p>');
+        });
+        if ($(data.feed.entry).size() == 50) { getComments(commentsURL, startIndex + 50); }
+        }
+      });
+    }
+
+    getYouTubeInfo();
+
+  });
+
 });    
